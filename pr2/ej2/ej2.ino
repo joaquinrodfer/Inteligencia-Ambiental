bda7f54d396b61e4bf6cdba1bf502b0f78d38f8c
joaquinrodfer <joaquinecazorla@gmail.com>
2024-04-10 12:23:11 +0200

    First Commit

---
+++ pr2/ej2/ej2.ino
@@ -0,0 +1,100 @@
+#include <SPI.h>
+#include <WiFi101.h>
+#include <MQTT.h>
+
+const char ssid[] = "TP-LINK_7794";
+const char pass[] = "00280549";
+bool led1 = false;
+boolean led2 = false;
+String numero;
+WiFiClient net;
+MQTTClient client;
+unsigned long lastMillis = 0;
+
+const char broker[] = "192.168.0.100";
+int port = 1883;
+const char topic1[] = "A3-467/puesto6/led1";
+const char topic2[] = "A3-467/puesto6/led2";
+
+void connect() {
+    Serial.print("checking wifi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println("\nConnected to WiFi!");
+
+    Serial.print("\nConnecting to broker...\n");
+   // while(!mqttClient.connect(broker, port)) {
+     // Serial.print("MQTT failed\n");
+     // delay(5000);
+    //}
+
+    while (!client.connect("mkr1000")){
+        Serial.print(".");
+        delay(1000);
+    }
+
+
+    Serial.println("\nConnected!");
+    client.subscribe("A3-467/puesto6/led1");
+    client.subscribe("A3-467/puesto6/led2");
+}
+
+void setup() {
+  Serial.begin(9600);
+    
+    while (!Serial) {
+        delay(10);
+    }
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+    Serial.println("Iniciando conexión wifi");
+    WiFi.begin(ssid, pass);
+
+    client.begin(broker,net);
+    client.onMessage(messageReceived);
+    
+    connect();
+}
+
+void messageReceived(String &topic, String &payload) {
+        if (payload == "1;") {
+          if (led1 == false){
+            digitalWrite(A0, HIGH);
+            client.publish("A3-467/puesto6/led1", ";");
+            Serial.println("Encendiendo Led1");
+            led1=true;
+          }else{
+            client.publish("A3-467/puesto6/led1", ";");
+            Serial.println("Apagando Led1");
+            digitalWrite(A0, LOW);
+            led1=false;
+          }
+        }
+        if (payload == "2;") {
+          if (led2 == false){
+            digitalWrite(A1, HIGH);
+            
+            client.publish("A3-467/puesto6/led2", ";");
+            Serial.println("Encendiendo Led2");
+            led2=true;
+          }else{
+            client.publish("A3-467/puesto6/led2", ";");
+            Serial.println("Apagando Led2");
+            digitalWrite(A1, LOW);       
+            led2=false;
+          }
+        }
+    
+}
+
+
+
+void loop() {
+    client.loop();
+    if (!client.connected()) {
+        connect();
+        delay(1000);
+    }    
+}
---
+++ pr2/ej2/ventana.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import paho.mqtt.client as mqtt
+
+# Configuración MQTT
+broker_address = "192.168.0.100"
+port = 1883
+user = "TP-LINK_7794"
+password = "00280549"
+
+def enviar_mensaje(led, estado):
+    topic = f"A3-467/puesto6/led{led}"
+    print(topic)
+    client.publish(topic, estado)
+
+def encender_led1():
+    enviar_mensaje("1", "1;")
+
+def encender_led2():
+    enviar_mensaje("2", "2;")
+
+
+
+# Creamos una ventana
+root = tk.Tk()
+root.title("Control de LEDs")
+root.geometry("200x100")
+
+
+# Botones para LED 1
+btn_led1_on = tk.Button(root, text="Modificar LED 1", command=encender_led1)
+btn_led1_on.pack()
+
+# Botones para LED 2
+btn_led2_on = tk.Button(root, text="Modificar LED 2", command=encender_led2)
+btn_led2_on.pack()
+
+# Configurar cliente MQTT
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "puesto6")
+
+client.connect(broker_address, port)
+client.loop_start()
+
+root.mainloop()
